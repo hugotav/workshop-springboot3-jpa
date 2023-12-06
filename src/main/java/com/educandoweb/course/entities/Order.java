33e0c1c3c87f11b22ee3b4dbb85bb865b1058de9
Hugo Tavares <hugotavaresneto@gmail.com>
2023-12-06 09:59:40 -0300

    subindo novas alterações

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -39,7 +39,7 @@ public class Order implements Serializable {
 	private User client;
 	
 	@OneToMany(mappedBy = "id.order")
-	private Set<OrderItem> items = new HashSet<>();	
+	private Set<OrderItem> items = new HashSet<>();		
 
 	public Order() {
 
@@ -86,6 +86,10 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
+
+	public Set<OrderItem> getItems(){
+		return items;
+	} 
 
 	@Override
 	public int hashCode() {
--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -33,6 +33,7 @@ public class User implements Serializable {
 	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
+		
 	}
 
 	public User(Long id, String name, String email, String phone, String password) {
